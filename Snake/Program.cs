f3f3a0d456136e770819370e55a85d0aba62a234
Bawarez <bavarets@tut.by>
2015-10-07 20:11:03 +0200

    1

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,6 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            int y = 3;
+            Show(x, y, '*');
+            x = 4;
+            y = 6;
+            Show(x, y, '#');
+            Console.ReadLine();
+
+        }
+
+        static void Show(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write(c);
         }
     }
 }
